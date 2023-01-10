ea1a7aa7510d6744af73ce5b51fa713d6e7ce5d4
Robert Zane <rob.zane24@gmail.com>
2023-01-10 08:38:22 -0600

    Completed JavaScript_Variables

--- app.js
+++ app.js
@@ -4,7 +4,36 @@ console.log(
 );
 
 // Exercise 1
+const firstname = "Robbie";
+let lastname = "Zan";
+var age = 30;
 
 // Exercise 2
+lastname = "Zane";
+age = 31;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log('${language} was first released in ${createdYear}.');
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log('${product} in size ${selectedSize} is ${price} with ${stock} in stock.');
+
+// Exercise 5
+let title = "Name of the wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
+
+console.log('${title} by ${author} is ${pageCount} pages long.')
